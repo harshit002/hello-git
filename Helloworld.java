6a60e7e83aedf0889bc7c94c1aa3ffc65f0a2a72
harshit002 <harshit.asthanabd@gmail.com>
2018-06-06 16:25:30 +0530

    first commit

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+package assignment23;
+
+public class Helloworld {
+	 
+	public static void main(String args[])
+	{
+		System.out.println("hello world");
+	}
+
+}
---
+++ name.java
@@ -0,0 +1,11 @@
+package assignment23;
+
+public class name {
+	public static void main(String args[])
+	{
+		System.out.println("Harshit Asthana");
+	}
+
+}
+
+
