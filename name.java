ab30c9e8f98a2805c716a1f79c2ab60820f5b9ea
harshit002 <36203721+harshit002@users.noreply.github.com>
2018-06-06 17:40:07 +0530

    Update name.java

--- name.java
+++ name.java
@@ -3,7 +3,7 @@ package assignment23;
 public class naming {
 	public static void main(String args[])
 	{
-		System.out.println("Harshit Asthana");
+		System.out.println("Harshit Asthana DIT");
 	}
 
 }
